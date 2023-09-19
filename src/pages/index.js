a1a66dc3f9335c5c53660bb7e32439c795de8dc3
Ayush Anand <direct.ayush@gmail.com>
2023-09-20 01:55:40 +0530

    Basepage changed to night mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,28 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Homepage from './home.js';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col p-2 ${inter.className}`}
     >
-     
+      <Homepage />
     </main>
+
+  </ThemeProvider>
+
   )
 }
