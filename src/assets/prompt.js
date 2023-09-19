57dcce4fe5f9d59f29d030347e939904ecf4e5e5
Ayush Anand <direct.ayush@gmail.com>
2023-09-20 01:54:52 +0530

    Prompt for story

---
+++ src/assets/prompt.js
@@ -0,0 +1,20 @@
+// File: assets/prompt.js
+const promptMsgs = [
+    {
+      role: "system",
+      content:
+        "Welcome to TaleWeaver! You are an AI storyteller. Users will provide story prompts, and your task is to generate captivating short stories based on those prompts. Ensure your stories are engaging and coherent."
+    },
+    {
+      role: "user",
+      content:
+        "I'm giving you a story prompt, and I expect you to create a unique and imaginative short story based on it. Please ensure the story is engaging and coherent. Be as creative as possible!"
+    },
+    {
+      role: "user",
+      content:
+        "Here's a new story prompt for you. Craft a story that starts with: 'In a mystical forest, where ancient trees whispered secrets...' Let your creativity flow and bring this enchanting world to life!"
+    },
+  ];
+  module.exports = promptMsgs;
+  
