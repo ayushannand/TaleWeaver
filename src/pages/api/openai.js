c0808492e2fb62bc1891fee53416c0693f0ada24
Ayush Anand <direct.ayush@gmail.com>
2023-09-21 01:22:47 +0530

    openai error handelling

--- src/pages/api/openai.js
+++ src/pages/api/openai.js
@@ -5,30 +5,29 @@ const openai = new OpenAI({
 });
 
 export default async function handler(req, res) {
-  const { prompt,length } = req.body;
+  try {
+    const { prompt, length } = req.body;
 
-  const instructions = require("../../assets/prompt.js");
+    const instructions = require("../../assets/prompt.js");
 
+    // Define or replace userPrompt with the actual user prompt
+    const userPrompt = {
+      role: "user",
+      content: `${prompt}\n**Keep the story in about ${length} words.** Strictly follow the word count estimate and don't go beyond 110% of length or don't make it 90% of the length`,
+    };
 
-  // Define or replace userPrompt with the actual user prompt
-  const userPrompt = {
-    role: "user",
-    content: `${prompt}\n**Keep the story in about ${length} words.** Strictly follow the word count estimate and don't go beyond 110% of length or don't make it 90% of the length`,
-  };
+    const msgs = [...instructions, userPrompt]; // Combine instructions with user prompt
 
+    const response = await openai.chat.completions.create({
+      messages: msgs,
+      model: "gpt-3.5-turbo",
+    });
 
-  const msgs = [...instructions, userPrompt]; // Combine instructions with user prompt
+    const responseData = response.choices[0].message.content; // Extract the response content as a string
 
-  const response = await openai.chat.completions.create({
-    messages: msgs,
-    model: "gpt-3.5-turbo",
-});
-const responseData = response.choices[0].message.content; // Extract the response content as a string
-
-console.log(responseData); // Log the response content
-
-res.status(200).send(responseData); 
-
-
-
+    res.status(200).send(responseData);
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).send("An error occurred while processing your request.");
+  }
 }
